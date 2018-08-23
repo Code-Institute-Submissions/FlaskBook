edc767e5abe013302e79f347bdd57ee7e29fce8d
Dave O'Dea <daveodea@gmail.com>
2018-08-23 18:56:31 +0100

    Fix missing env vars

--- app.py
+++ app.py
@@ -33,4 +33,6 @@ def insert_recipe():
     
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
