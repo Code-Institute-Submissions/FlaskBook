1242c8f424ce2943ff65bdb48db3a0344b2ddaca
Dave O'Dea <daveodea@gmail.com>
2018-08-24 11:08:00 +0100

    Add extra further fields form

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
 @app.route('/')
+# Get all recipes
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", 
     recipes=mongo.db.recipes.find())
 
-
+# Generate HTML template
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html',
@@ -24,15 +25,22 @@ def add_recipe():
     allergens=mongo.db.allergens.find(),
     cuisines=mongo.db.cuisine.find(),)
     
-    
+# Send the form
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes =  mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
+    form_dict = dict()
+    form_dict["title"] =  request.form["title"]
+    form_dict["cuisine"] =  request.form["cuisine"]
+    mylist = request.form.to_dict('allergenlist')
+    form_dict["allergenlist"] = mylist
+    recipes.insert(form_dict)
+    # form_dict["allergenlist"] =  request.form["allergenlist"]
+    # print(form_dict)
+    # multi_result = request.form["allergenlist"].to_dict(flat=False)
+    # form_dict["allergenlist"] =  multi_result
     return redirect(url_for('get_recipes'))
     
     
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+    app.run(debug=True)
